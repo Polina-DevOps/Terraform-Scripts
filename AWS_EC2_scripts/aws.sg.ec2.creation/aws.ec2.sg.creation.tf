dd70577574b0d937a455a240b951090fe5222346
Nageswar Polina <polinadevops2021@gmail.com>
2021-07-25 01:14:20 +0800

    Terraform AWS EC2 v1.2

--- AWS_EC2_scripts/aws.sg.ec2.creation/aws.ec2.sg.creation.tf
+++ AWS_EC2_scripts/aws.sg.ec2.creation/aws.ec2.sg.creation.tf
@@ -16,16 +16,16 @@ resource "aws_security_group" "allow_tcp_all" {
 
   ingress {
     description     = "SSH from VPC"
-    from_port       = 0
-    to_port         = 0
+    from_port       = 22
+    to_port         = 22
     protocol        = "tcp"
     cidr_blocks     = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = -1
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
     cidr_blocks     = ["0.0.0.0/0"]
   }
 
