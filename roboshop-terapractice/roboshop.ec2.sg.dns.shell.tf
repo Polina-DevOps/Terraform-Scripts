42aa1caf0f65729503d03257dc51cb4833ed1512
Nageswar Polina <polinadevops2021@gmail.com>
2021-07-28 01:25:10 +0800

    Terraform automations practicev1.9.5

--- roboshop-terapractice/roboshop.ec2.sg.dns.shell.tf
+++ roboshop-terapractice/roboshop.ec2.sg.dns.shell.tf
@@ -9,7 +9,7 @@ resource "aws_spot_instance_request" "cheap_worker" {
   vpc_security_group_ids = [aws_security_group.allow_roboshop_tcp]
 
   tags = {
-    Name = element(var.SERVERS_VAR,count.index )
+    Name = element(var.SERVERS_VAR,count.index)
   }
 }
 
@@ -43,9 +43,9 @@ resource "aws_security_group" "allow_roboshop_tcp" {
 resource "aws_ec2_tag" "robhop_server_tags" {
   depends_on =       [aws_route53_record.roboshop_DNS_Ser]
   count              = length(var.SERVERS_VAR)
-  resource_id        = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id,count.index )
+  resource_id        = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id,count.index)
   key                = "Name"
-  value              = element(var.SERVERS_VAR,count.index )
+  value              = element(var.SERVERS_VAR,count.index)
 }
 
 ## To define route53 DNS entries
@@ -53,7 +53,7 @@ resource "aws_ec2_tag" "robhop_server_tags" {
 resource "aws_route53_record" "roboshop_DNS_Ser" {
   count              = length(var.SERVERS_VAR)
   zone_id            = "Z039980724SLMJM27D0IM"
-  name               = element(var.SERVERS_VAR,count.index )
+  name               = element(var.SERVERS_VAR,count.index)
   type               = "A"
   ttl                = "300"
   records            = [element(aws_spot_instance_request.cheap_worker.*.private_ip,count.index)]
@@ -66,7 +66,7 @@ resource "null_resource" "shellscript_define" {
     count            = length(var.SERVERS_VAR)
     connection {
       type           = "ssh"
-      host           = element(aws_spot_instance_request.cheap_worker.*.private_ip,count.index )
+      host           = element(aws_spot_instance_request.cheap_worker.*.private_ip,count.index)
       user           = "centos"
       password       = "DevOps321"
     }
@@ -74,7 +74,7 @@ resource "null_resource" "shellscript_define" {
       "cd /home/centos",
       "git clone https://github.com/Polina-DevOps/Shell-Scripts.git",
       "cd /home/Shell-Scripts/Roboshop-Init",
-      "sudo make ${element(var.SERVERS_VAR,count.index )}"
+      "sudo make ${element(var.SERVERS_VAR,count.index)}"
     ]
   }
 }
