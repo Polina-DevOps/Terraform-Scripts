28fd4654d318d0ee70af1b3539b21ddfdff431f8
Nageswar Polina <polinadevops2021@gmail.com>
2021-07-28 01:47:34 +0800

    Terraform automations practicev1.9.7.3

--- roboshop-terapractice/roboshop.ec2.sg.dns.shell.tf
+++ roboshop-terapractice/roboshop.ec2.sg.dns.shell.tf
@@ -66,10 +66,10 @@ resource "null_resource" "shellscript_define" {
     depends_on       = [aws_route53_record.roboshop_DNS_Ser]
       provisioner "remote-exec" {
         connection {
-        type           = "ssh"
-        host           = element(aws_spot_instance_request.cheap_worker.*.private_ip,count.index)
-        user           = "centos"
-        password       = "DevOps321"
+            type           = "ssh"
+            host           = element(aws_spot_instance_request.cheap_worker.*.private_ip,count.index)
+            user           = "centos"
+            password       = "DevOps321"
     }
     inline = [
         "cd /home/centos",
