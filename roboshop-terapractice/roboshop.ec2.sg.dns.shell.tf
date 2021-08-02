2f2b3f340734af959a0613a7b7e561ca76c069eb
Nageswar Polina <polinadevops2021@gmail.com>
2021-08-02 23:58:08 +0800

    Terraform automations practicev1.9.9.5

--- roboshop-terapractice/roboshop.ec2.sg.dns.shell.tf
+++ roboshop-terapractice/roboshop.ec2.sg.dns.shell.tf
@@ -3,7 +3,7 @@
 resource "aws_spot_instance_request" "cheap_worker" {
   count                   = length(var.MYSERVERS)
   ami                     = "ami-074df373d6bafa625"
-  spot_price              = "0.03"
+  spot_price              = "0.0035"
   instance_type           = "t2.micro"
   wait_for_fulfillment    = true
   vpc_security_group_ids  = [aws_security_group.allow_roboshop_tcp.id]
